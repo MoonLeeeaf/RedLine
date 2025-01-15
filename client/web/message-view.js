d0333c0fbc78890db1319b6469b6cd496bb72da9
MoonLeeeaf <150461955+MoonLeeeaf@users.noreply.github.com>
2025-01-16 00:32:13 +0800

    喵喵喵

--- client/web/message-view.js
+++ client/web/message-view.js
@@ -1,6 +1,7 @@
+// todo: 写成promise
 let template_id = 'message-view-template'
-$(document.body).append($.parseHTML(`<div id="${template_id}"`))
-$('#' + template_id).load('template.html')
+$(document.body).append($.parseHTML(`<div id="${template_id}"></div>`))
+$('#' + template_id).load('message-view-template.html')
 
 class MessageNormal extends HTMLElement {
     static observedAttributes = ['avatar', 'sender-name', 'sender-id', 'msg', 'direction']
@@ -117,7 +118,7 @@ customElements.define('message-img', class extends HTMLElement {
             $(this).attr('alt', '图像损坏')
         }
         e.onclick = () => {
-            import('../image-viewer-dialog/index.js').then((m) => m.default($(this).attr('src')))
+            import('image-viewer-dialog.js').then((m) => m.default($(this).attr('src')))
         }
         this.appendChild(e)
     }
